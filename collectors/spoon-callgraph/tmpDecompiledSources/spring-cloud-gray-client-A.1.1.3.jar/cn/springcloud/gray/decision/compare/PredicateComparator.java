171da41a23da19a52ad6cd9081c77c11acd2bbff
Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>
2020-05-06 01:42:37 +0100

    remove folde

--- collectors/spoon-callgraph/tmpDecompiledSources/spring-cloud-gray-client-A.1.1.3.jar/cn/springcloud/gray/decision/compare/PredicateComparator.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.146.
- */
-package cn.springcloud.gray.decision.compare;
-
-public interface PredicateComparator<T> {
-    public boolean test(T var1, T var2);
-}
-
--- collectors/spoon-callgraph/tmpDecompiledSources/spring-cloud-gray-server-A.1.1.3.jar/cn/springcloud/gray/server/module/gray/GrayInstanceRecordEvictor.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.146.
- */
-package cn.springcloud.gray.server.module.gray;
-
-public interface GrayInstanceRecordEvictor {
-    public void evict();
-}
-
