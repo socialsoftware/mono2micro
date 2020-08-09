1e6212945d6becb25afdc4ba8d082ad9deb54549
berhart <bernardo.andrade@tecnico.ulisboa.pt>
2020-08-09 18:27:32 +0100

    added print and lineno util functions

--- backend/src/main/java/pt/ist/socialsoftware/mono2micro/utils/Utils.java
+++ backend/src/main/java/pt/ist/socialsoftware/mono2micro/utils/Utils.java
@@ -5,12 +5,18 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
+
+    public static Integer lineno() { return new Throwable().getStackTrace()[1].getLineNumber(); }
+
+    public static void print(String message, Integer lineNumber) { System.out.println("[" + lineNumber + "] " + message); }
+
     public static List<String> getJsonFileKeys(InputStream is) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         JsonFactory jsonfactory = mapper.getFactory();
@@ -29,6 +35,7 @@ public class Utils {
             if (jsonToken == JsonToken.END_OBJECT)
                 break;
 
+            // FIXME Wrong! Starting with an array should not be an assumption.
             if (jsonToken == JsonToken.START_ARRAY && jsonParser.getCurrentName() != null) {
                 String controllerName = jsonParser.getCurrentName();
                 System.out.println("Controller name: " + controllerName);
