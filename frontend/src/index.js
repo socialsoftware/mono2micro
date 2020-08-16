30e463863e9b3c680896489966dbb68eb5a302d3
berhart <bernardo.andrade@tecnico.ulisboa.pt>
2020-08-16 22:55:35 +0100

    trigger conflict

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/table.css';
 
 ReactDOM.render(
-     <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <App />,
     document.getElementById('root')
 );
