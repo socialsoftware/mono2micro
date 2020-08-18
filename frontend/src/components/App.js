35346de53222f8edde301de953cbcf1d96653bce
berhart <bernardo.andrade@tecnico.ulisboa.pt>
2020-08-18 22:17:46 +0100

    removed unused import

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Header} from './Header';
 import { Main } from './Main';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import { BrowserRouter } from 'react-router-dom';
 
 
