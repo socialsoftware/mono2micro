bce0cbe66881985c4654b3e90f903da02645400f
berhart <bernardo.andrade@tecnico.ulisboa.pt>
2020-10-26 00:34:21 +0000

    removed unused imports

--- collectors/dynamic-collection/scripts/src/main/java/serializers/CompactedTraceSerializer.java
+++ collectors/dynamic-collection/scripts/src/main/java/serializers/CompactedTraceSerializer.java
@@ -2,8 +2,6 @@ package serializers;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import domain.CompactedTrace;
 
 import java.io.IOException;
 
