837e9419bd280bcb6d232f254f3a7e51712831ca
berhart <bernardo.andrade@tecnico.ulisboa.pt>
2020-11-06 23:35:48 +0000

    optimized and fixed entity view graph

--- frontend/src/components/view/EntityView.js
+++ frontend/src/components/view/EntityView.js
@@ -105,7 +105,6 @@ export class EntityView extends React.Component {
                     clustersControllers: response2.data,
                 }, () => {
                     let amountList = {};
-
                     const {
                         entities,
                         clusters,
@@ -114,7 +113,6 @@ export class EntityView extends React.Component {
                     for (var i = 0; i < entities.length; i++) {
                         let amount = 0;
                         let entityName = entities[i];
-
                         let entityCluster = clusters.find(c => c.entities.includes(entityName));
 
                         for (var j = 0; j < clusters.length; j++) {
@@ -148,13 +146,16 @@ export class EntityView extends React.Component {
     }
 
     //get controllers that access both an entity and another cluster
-    getCommonControllers(entityName, cluster) {
-        let entityControllers = this.state.controllers
-            .filter(controller => Object.keys(controller.entities).includes(entityName))
-            .map(c => c.name);
-
+    getCommonControllers(entity, cluster) {
+        let entityControllers = [];
         let clustersControllers = this.state.clustersControllers[cluster.name].map(c => c.name);
-        return entityControllers.filter(c => clustersControllers.includes(c));
+
+        this.state.controllers.forEach(controller => {
+            if (controller.entities[entity] && clustersControllers.includes(controller.name))
+                entityControllers.push(controller.name);
+        })
+
+        return entityControllers;
     }
 
     loadGraph() {
@@ -167,10 +168,15 @@ export class EntityView extends React.Component {
 
         let nodes = [];
         let edges = [];
-        let entityControllers = controllers
-            .filter(controller => Object.keys(controller.entities).includes(entity))
-            .map(controller => controller.name + " " + controller.entities[entity]);
 
+        
+        let entityControllers = [];
+        
+        controllers.forEach(controller => {
+            if (controller.entities[entity])
+                entityControllers.push(controller.name + " " + controller.entities[entity]);
+        })
+        
         nodes.push({
             id: entity,
             label: entity,
