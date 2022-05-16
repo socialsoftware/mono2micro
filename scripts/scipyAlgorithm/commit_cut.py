34fb3d24f790ee86ce04de00448c163e633af44c
João Lourenço <estudante.lourenco@gmail.com>
2022-05-16 11:02:34 +0100

    Add debugging prints

--- scripts/scipyAlgorithm/commit_cut.py
+++ scripts/scipyAlgorithm/commit_cut.py
@@ -1,9 +1,5 @@
 import numpy as np
 from scipy.cluster import hierarchy
-import sys
-np.set_printoptions(threshold=sys.maxsize)
-import matplotlib.pyplot as plt
-from collections import defaultdict
 
 
 def cut(codebases_path, codebase_name, commit_metric_value, authors_metric_value, n_clusters, similarityMatrixData):
@@ -11,24 +7,31 @@ def cut(codebases_path, codebase_name, commit_metric_value, authors_metric_value
     entities = similarityMatrixData["entities"]
     linkageType = similarityMatrixData["linkageType"]
     matrix = similarityMatrixData["matrix"]
+    # print(matrix)
+    # print(len(matrix))
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             matrix[i][j] = matrix[i][j][0] * int(commit_metric_value) / 100 + \
                            matrix[i][j][1] * int(authors_metric_value) / 100
 
     matrix = np.array(matrix)
-    # print(matrix)
+    np.savetxt(codebases_path + codebase_name + "/" + f"{commit_metric_value},{authors_metric_value},{n_clusters}" + "/singlecutmatrix.out", matrix)
+    print(matrix)
     hierarc = hierarchy.linkage(y=matrix, method=linkageType)
 
-    cut_result = hierarchy.cut_tree(hierarc, n_clusters=n_clusters)
+    #cut_result = hierarchy.cut_tree(hierarc, n_clusters=n_clusters)
     fcluster_clusters = hierarchy.fcluster(hierarc, n_clusters, criterion='maxclust')
     # print(entities)
+    # print(matrix.shape)
     clusters = {}
+    # print(fcluster_clusters)
+    # print(f"{len(fcluster_clusters)} elements in fcluster clusters")
     for i in range(len(fcluster_clusters)):
         if str(fcluster_clusters[i]) in clusters.keys():
             clusters[str(fcluster_clusters[i])] += [entities[i]]
         else:
             clusters[str(fcluster_clusters[i])] = [entities[i]]
+    print(clusters)
 
     # print(fcluster_clusters)
     # print(cut_result)
