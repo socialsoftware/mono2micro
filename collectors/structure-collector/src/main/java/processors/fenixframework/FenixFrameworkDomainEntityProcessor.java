3bb44231c84f9fb6361b66a554af61bb8ce638c6
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2023-10-02 13:12:59 +0100

    Base fenix framework field collection on methods

--- collectors/structure-collector/src/main/java/processors/fenixframework/FenixFrameworkDomainEntityProcessor.java
+++ collectors/structure-collector/src/main/java/processors/fenixframework/FenixFrameworkDomainEntityProcessor.java
@@ -1,9 +1,15 @@
 package processors.fenixframework;
 
+import domain.Field;
+import domain.datatypes.DataTypeFactory;
 import processors.DomainEntityProcessor;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.reference.CtTypeReference;
 import collectors.DomainEntityCollector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FenixFrameworkDomainEntityProcessor extends DomainEntityProcessor {
 
@@ -17,5 +23,25 @@ public class FenixFrameworkDomainEntityProcessor extends DomainEntityProcessor {
     public boolean isToBeProcessed(CtClass<?> candidate) {
         CtTypeReference<?> superclass = candidate.getSuperclass();
         return superclass != null && superclass.getSimpleName().endsWith(DOMAIN_ENTITY_SUPERCLASS_SUFFIX);
+    }
+
+    @Override
+    protected List<Field> processFields(CtClass<?> ctClass) {
+        List<Field> fields = new ArrayList<>();
+        for (CtMethod<?> ctMethod : ctClass.getSuperclass().getDeclaration().getMethods()) {
+            if (ctMethod.getSimpleName().startsWith("get")) { // TODO might need to filter out API fields
+                fields.add(processAccessorMethod(ctMethod));
+            }
+        }
+        return fields;
+    }
+
+    private Field processAccessorMethod(CtMethod<?> ctMethod) {
+        return new Field(getFieldName(ctMethod), DataTypeFactory.getOrCreateDataType(ctMethod.getType()));
+    }
+
+    private String getFieldName(CtMethod<?> ctMethod) {
+        String fieldName = ctMethod.getSimpleName().substring(3);
+        return fieldName.substring(0, 1).toLowerCase() + fieldName.substring(1);
     }
 }
